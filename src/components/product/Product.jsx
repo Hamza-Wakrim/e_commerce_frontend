384b7cd718b60e4a9ccda4c39e8b60256dd0a4a1
achrafedd <achrafeddiba@gmail.com>
2022-08-07 16:42:24 +0100

    fix git shit

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,18 +1,15 @@
-import React, {useContext} from "react";
-import {Link, useNavigate} from "react-router-dom";
-import {AiOutlineShoppingCart, AiFillStar} from "react-icons/ai";
-import {FaShoppingCart} from "react-icons/fa";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AiOutlineShoppingCart, AiFillStar } from "react-icons/ai";
 import axios from "axios";
-import {productsContext} from "../../App";
+import { productsContext } from "../../App";
 import "./product.css";
 
-const Product = ({product: {id, name, price, media}, rating, button}) => {
+const Product = ({ product: { id, name, price, media }, rating, button }) => {
     const productURL = `/product/${name}`;
-<<<<<<< HEAD
+
     const { user, getCartProducts } = useContext(productsContext);
-=======
-    const {user} = useContext(productsContext);
->>>>>>> a72f18d19d9ec9bb4ff4f1b65877e077b51990c6
+
     const navigate = useNavigate();
     const addToCart = () => {
         if (user) {
@@ -23,7 +20,7 @@ const Product = ({product: {id, name, price, media}, rating, button}) => {
             );
             getCartProducts();
         } else {
-            navigate("/login", {replacea: true});
+            navigate("/login", { replacea: true });
         }
     };
 
@@ -33,15 +30,15 @@ const Product = ({product: {id, name, price, media}, rating, button}) => {
                 {name}
             </Link>
             <Link to={productURL}>
-                <img src={media[0].url} alt="" className="image"/>
+                <img src={media[0].url} alt="" className="image" />
             </Link>
             <div className="rating">
                 <div className="stars">
-                    <AiFillStar className="icon-star"/>
-                    <AiFillStar className="icon-star"/>
-                    <AiFillStar className="icon-star"/>
-                    <AiFillStar className="icon-star"/>
-                    <AiFillStar className="icon-star"/>
+                    <AiFillStar className="icon-star" />
+                    <AiFillStar className="icon-star" />
+                    <AiFillStar className="icon-star" />
+                    <AiFillStar className="icon-star" />
+                    <AiFillStar className="icon-star" />
                 </div>
                 <div className="rate">{rating}</div>
             </div>
@@ -53,15 +50,12 @@ const Product = ({product: {id, name, price, media}, rating, button}) => {
                 ""
             )}
             {button ? (
-<<<<<<< HEAD
-                <button onClick={() => addToCart()} className="add-to-cart">
-                    <AiOutlineShoppingCart className="icon" />
-=======
-                <button onClick={addToCart} className="add-to-cart">
-                    <FaShoppingCart/>
->>>>>>> a72f18d19d9ec9bb4ff4f1b65877e077b51990c6
-                    <span>ADD TO CART</span>
-                </button>
+                <div>
+                    <button onClick={addToCart} className="add-to-cart">
+                        <AiOutlineShoppingCart className="icon" />
+                        <span>ADD TO CART</span>
+                    </button>
+                </div>
             ) : (
                 ""
             )}
